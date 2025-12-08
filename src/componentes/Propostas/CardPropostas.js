70f84b88f3c6e9d62d3d55d684cda2e9d923f64b
Luiz Filipe Bruck Reis Mattos <lfbruckmattos@gmail.com>
2025-12-08 16:00:42 -0300

    Cards finalizados

--- src/componentes/Propostas/CardPropostas.js
+++ src/componentes/Propostas/CardPropostas.js
@@ -3,7 +3,7 @@ export default function CardPropostas({imagem,titulo,descricao}){
     return(
         <div className="cardProposta">
             <img src={imagem} alt={"Imagem da Proposta"}/>
-            <h3>{titulo}</h3>
+            <h3 id="tituloProposta">{titulo}</h3>
             <p>{descricao}</p>
         </div>
     )
