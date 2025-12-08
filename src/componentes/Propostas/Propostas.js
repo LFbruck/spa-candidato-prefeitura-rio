c98415bb607233fab7903bd700669c9db22f657a
Luiz Filipe Bruck Reis Mattos <lfbruckmattos@gmail.com>
2025-12-07 23:19:30 -0300

    Construindo componente (propostas)

--- src/componentes/Propostas/Propostas.js
+++ src/componentes/Propostas/Propostas.js
@@ -2,4 +2,15 @@ import "./Propostas.css";
 
 export default function Propostas(){
 
+    return (
+        <section className="propostas">
+            <h2>Propostas</h2>
+            <div className="cardProposta">
+                <img/>
+                <h3>Titulo Proposta</h3>
+                <p>Descricao proposta</p>
+            </div>
+        </section>
+    )
+
 }
