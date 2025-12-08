7ff123f01f8a1adfce1212523b753cfee281fb6e
Luiz Filipe Bruck Reis Mattos <lfbruckmattos@gmail.com>
2025-12-07 21:46:45 -0300

    Menu sanduiche para mobile

--- src/componentes/Header/Header.js
+++ src/componentes/Header/Header.js
@@ -1,16 +1,26 @@
 import "./Header.css";
+import {useState} from "react";
 
 export default function Header(
 ){
+
+    const[menuAberto, setMenuAberto] = useState(false)  ;
+    const toggleMenu = () => {
+        setMenuAberto(!menuAberto);
+    };
+
     return (
         <header className="header">
             <h1 className="titulo"> Marco Madureira</h1>
-            <nav className="nav">
-                <a href="#biografia">Biografia</a>
-                <a href="#propostas">Propostas</a>
-                <a href="#agenda">Agenda</a>
-
+            <button className="menuToggle" onClick={toggleMenu}>☰
+            </button>
+            <nav className={`nav ${menuAberto ? 'nav-aberto' : ''}`}>
+                <a href="#biografia" onClick={toggleMenu}>Biografia</a>
+                <a href="#propostas" onClick={toggleMenu}>Propostas</a>
+                <a href="#agenda" onClick={toggleMenu}>Agenda</a>
+                <a href="#contato" onClick={toggleMenu}>Contatos</a>
             </nav>
+
 
         </header>
     )
