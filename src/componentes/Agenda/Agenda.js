f85983f4af5299d904e88d0ca078524a9071259a
Luiz Filipe Bruck Reis Mattos <lfbruckmattos@gmail.com>
2025-12-08 23:33:57 -0300

    Criação do componente Agenda

--- src/componentes/Agenda/Agenda.js
+++ src/componentes/Agenda/Agenda.js
@@ -1,5 +1,60 @@
 import "./Agenda.css";
 
-export default function Agenda(){
+export default function Agenda() {
+    return (
+        <section className="agenda" id="agenda">
+            <h2>Agenda de Campanha</h2>
 
+            <div className="eventosContainer">
+
+                <div className="evento">
+                    <div className="eventoDataBox">
+                        <span className="dia">15</span>
+                        <span className="mes">DEZ</span>
+                    </div>
+                    <div className="evento-detalhes">
+                        <h3>Debate sobre Educação</h3>
+                        <p className="horario">🕐 14h00</p>
+                        <p className="local">📍 Praça XV - Centro</p>
+                    </div>
+                </div>
+                <div className="evento">
+                    <div className="eventoDataBox">
+                        <span className="dia">15</span>
+                        <span className="mes">DEZ</span>
+                    </div>
+                    <div className="evento-detalhes">
+                        <h3>Debate sobre Educação</h3>
+                        <p className="horario">🕐 14h00</p>
+                        <p className="local">📍 Praça XV - Centro</p>
+                    </div>
+                </div>
+                <div className="evento">
+                    <div className="eventoDataBox">
+                        <span className="dia">15</span>
+                        <span className="mes">DEZ</span>
+                    </div>
+                    <div className="evento-detalhes">
+                        <h3>Debate sobre Educação</h3>
+                        <p className="horario">🕐 14h00</p>
+                        <p className="local">📍 Praça XV - Centro</p>
+                    </div>
+                </div>
+                <div className="evento">
+                    <div className="eventoDataBox">
+                        <span className="dia">15</span>
+                        <span className="mes">DEZ</span>
+                    </div>
+                    <div className="evento-detalhes">
+                        <h3>Debate sobre Educação</h3>
+                        <p className="horario">🕐 14h00</p>
+                        <p className="local">📍 Praça XV - Centro</p>
+                    </div>
+                </div>
+
+
+
+            </div>
+        </section>
+    );
 }
